3e335641a8dcf1ab23b22c86c29c00ac448dd878
AviralChawla20 <aviralchawla02@gmail.com>
2024-04-24 02:42:47 +0530

    fixed chat

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -1,10 +1,14 @@
 // components/Chat.tsx
 
 import SendbirdApp from '@sendbird/uikit-react/App';
+import { getMaxAge } from 'next/dist/server/image-optimizer';
 import React from 'react';
-const email = localStorage.getItem("email");
+const email = localStorage.getItem("email") || "";
+const parts = email.split("@");
+const result = parts[0]
 const APP_ID = "43F443C3-8B99-437A-9B02-57BE973CFFA9";
-const USER_ID = email || "";
+const USER_ID = result;
+console.log(USER_ID);
 
 const Chat: React.FC = () => {
     return (
